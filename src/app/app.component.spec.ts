197885e9dc39ede8e19f0a48fe5842b839346ba8
Kevin <kb5ux@virginia.edu>
2023-08-05 19:25:45 -0400

    Styling Changes + Logo Image Upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LogisticsWebApp'`, () => {
+  it(`should have as title 'JE&BG SOLUTIONS, LLC'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LogisticsWebApp');
+    expect(app.title).toEqual('JE&BG SOLUTIONS, LLC');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LogisticsWebApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('JE&BG SOLUTIONS, LLC app is running!');
   });
 });
